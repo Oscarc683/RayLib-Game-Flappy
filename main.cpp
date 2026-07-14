a23f7c0465dc10a484a481b0a151acff49ae0979
Oscar Cruz Miguel <rihzcrosiont@gmail.com>
2026-07-14 11:55:32 -0700

    main build fix

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     Color skyBlue = {135, 206, 250, 255};
     InitWindow(300, 600, "FlappyMon");//Initializes a window canvas that the game's graphics
     SetTargetFPS(60);//Sets the game to run at 60fps
-    while(WindowShouldClose() == false){//checks 
+    while(WindowShouldClose() == false){//checks if user presses esc
         BeginDrawing();
         ClearBackground(skyBlue);
         EndDrawing();
